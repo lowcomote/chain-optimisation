af7161d68745bb0868aaa347bfc1f163a84655f3
Qurat ul ain <quratulain.ali@york.ac.uk>
2021-07-19 12:02:52 +0100

    removed unused code

--- org.eclipse.epsilon.etl.chain.selection/src/org/eclipse/epsilon/etl/chain/selection/Chain_MT.java
+++ org.eclipse.epsilon.etl.chain.selection/src/org/eclipse/epsilon/etl/chain/selection/Chain_MT.java
@@ -1,28 +1,15 @@
 package org.eclipse.epsilon.etl.chain.selection;
 
-import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.epsilon.emc.emf.EmfModel;
-import org.eclipse.epsilon.eol.IEolModule;
-import org.eclipse.epsilon.eol.models.IModel;
-import org.eclipse.epsilon.etl.EtlModule;
-import org.eclipse.epsilon.etl.launch.EtlRunConfiguration;
 
 public class Chain_MT {
 	public static void main(String[] args) throws Exception {
 		
 		Path modelsRoot = Paths.get("models");
 		Path metamodelsRoot = Paths.get("metamodels");
-		//Path scriptRoot = Paths.get("scripts");
 		Path genmodelsRoot = Paths.get("models/generatedmodels");
-		
-		//File metamodelPath = new File("metamodels");
-		//String contents[] = metamodelPath.list();
-
 		Chaining_MT chainingmt = new Chaining_MT();
 		
 		String sourcemodel=modelsRoot.resolve("Tree.xmi").toAbsolutePath().toUri().toString();
@@ -30,10 +17,8 @@ public class Chain_MT {
 		String targetMM = metamodelsRoot.resolve("DB.ecore").toString();
 		String targetmodel= genmodelsRoot.resolve("Gen_Graph20.xmi").toAbsolutePath().toUri().toString();
 		
-		ArrayList<String> bestchain = chainingmt.identifybestchain(sourcemodel, sourceMM, targetmodel, targetMM);
+		ArrayList<String> bestchain = chainingmt.identifyBestChain(sourcemodel, sourceMM, targetmodel, targetMM);
 		System.out.println("Best chain: "+bestchain);
 	}
-		
-	//}
 
 }
